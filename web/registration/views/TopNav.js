367e5daeb219da0dc5fc9968e2015a27091b79d6
Eivind Sommersten <eivind@machina.no>
2025-03-23 11:58:24 +0100

    minor additions to topnav

--- web/registration/views/TopNav.js
+++ web/registration/views/TopNav.js
@@ -5,21 +5,18 @@ export default class TopNav extends View {
     get clsasName() { return 'navbar fixed-top bg-body-tertiary'; }
     get template() {
         return /* html */ `
-            <div class="container-fluid">
+            <div class="container-fluid justify-content-center">
                 <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Active</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
-</ul>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#registrering">Registrering</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#konkurranse">Konkurranse</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#resultater">Resultater</a>
+                    </li>
+                </ul>
             </div>
         `;
     }
