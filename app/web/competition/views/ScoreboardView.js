d2539485aec669e1c481dace20b007f55ccc8325
Eivind <eivind@machina.no>
2025-11-18 13:01:27 +0100

    wip

--- app/web/competition/views/ScoreboardView.js
+++ app/web/competition/views/ScoreboardView.js
@@ -2,7 +2,6 @@ import View from "../../views/View.js";
 import SingleScoreView from "./SingleScoreView.js";
 
 export default class ScoreboardView extends View {
-    get className() { return 'scoreboard'; }
     initialize(options) {
         super.initialize(options);
         this.listenTo(app, 'window:focus', this.windowFocused);
@@ -25,6 +24,43 @@ export default class ScoreboardView extends View {
         const prev = from.el.previousElementSibling;
         this.focus(prev);
     }
+    updateMeta() {
+        const throws = this.model.get('throws');
+        let count = 0;
+        let sum = 0;
+        for (let i = 0; i < throws.length; i += 1) {
+            const sc = throws[i].score;
+            if (typeof sc === 'number') {
+                count += 1;
+                sum += sc;
+            }
+        }
+        const avg = count === 0 ? 0 : (sum / count);
+        this.$('.scoremeta .count').text(count);
+        this.$('.scoremeta .sum').text(sum);
+        this.$('.scoremeta .avg').text(avg.toFixed(1));
+    }   
+    get template() {
+        return /* html */ `
+            <div class="scoreboard"></div>
+            <div class="d-flex flex-column align-items-center my-2">
+                <div class="scoremeta d-flex align-items-center justify-content-between mt-3">
+                    <div class="d-flex flex-column text-start">
+                    <div class="badge text-start ps-0">Ant</div>
+                        <span class="count text-start">0</span>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <div class="badge">Avg</div>
+                        <span class="avg">-</span>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <div class="badge text-end pe-0">Sum</div>
+                        <span class="sum text-end">0</span>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
     render() {
         super.render();
         const throws = this.model.get('throws');
@@ -40,9 +76,11 @@ export default class ScoreboardView extends View {
             this.listenTo(ss, 'change:value', (sx) => {
                 this.focusNext(sx);
                 this.model.trigger('change');
+                this.updateMeta();
                 // this.sum();
             });
-            this.$el.append(ss.render().$el);
+            this.$('.scoreboard').append(ss.render().$el);
+            this.updateMeta();
         }
         setTimeout(() => {
             this.focus(this.items[0].$el);
